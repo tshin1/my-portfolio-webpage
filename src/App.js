9c2067716a2cdf085be02913e084f84ff853723e
Ted Shin <tdyshin@gmail.com>
2017-08-25 14:34:03 -0400

    Add materialize css and js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Ted's Portfolio</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p>
+          
         </p>
       </div>
     );
