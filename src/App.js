96955223005b21e80fe12c6a8b48f693cd467a29
Ted Shin <tdyshin@gmail.com>
2017-08-26 12:02:42 -0400

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+import NavBar from './components/NavBar';
+
+export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <NavBar />
+        <div>
           <h1>Ted's Portfolio</h1>
         </div>
         <p>
-          
+
         </p>
       </div>
     );
   }
 }
-
-export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a href="#" className="brand-logo">Logo</a>
+          <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><a href="#about">About</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
