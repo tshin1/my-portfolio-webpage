93526d2fc02e2a43eb2bb91bbdf13daef8e7d94b
Ted Shin <tdyshin@gmail.com>
2017-08-26 13:03:23 -0400

    Add profile image

--- src/App.js
+++ src/App.js
@@ -8,15 +8,31 @@ export default class App extends Component {
     return (
       <div>
         <NavBar />
-        <div className="container">
+        <div className="container" style={{ backgroundColor: "white" }}>
           <div className="row">
-            <h1>Ted Shin</h1>
+            <div className="col s8">
+              <h1>Ted Shin</h1>
+              <p>Full stack developer and CS student. Experience with React, React Native, ES6, Express, MongoDB, Sass, Webpack, Git, Django, Javascript, Python</p>
+            </div>
+            <div className="col s4">
+              <img
+                src="https://lh4.googleusercontent.com/nAN7y4JodBJESCzEiPOj26SxDv59n64d30R3453VvbWCgmImW2blGgWngjkbT1U8OIQKlAFoC7g_viE=w1600-h810-rw"
+                alt="Ted's Profile Image"
+                height="250"
+                width="250"
+                style={{ borderRadius: "50%" }}
+              />
+            </div>
           </div>
           <div className="row">
-
+            <div className="col s12">
+              <h1 className="center-align">Portfolio</h1>
+            </div>
           </div>
           <div className="row">
-
+            <div className="col s12">
+              <h1 className="center-align">Contact Me</h1>
+            </div>
           </div>
         </div>
       </div>
