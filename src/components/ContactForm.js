957b65ad5ee7336b0b78276a0a00d165c3894cf9
Ted Shin <tdyshin@gmail.com>
2017-09-21 16:32:25 -0400

    Change contact form to controolled component

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,39 +1,80 @@
 import React, { Component } from 'react';
 
 export default class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      message: '',
+    };
+  }
 
+  handleNameChange = (event) => {
+    this.setState({name: event.target.value});
+  }
+
+  handleEmailChange = (event) => {
+    this.setState({email: event.target.value});
+  }
+
+  handlePhoneChange = (event) => {
+    this.setState({phone: event.target.value});
+  }
+
+  handleMessageChange = (event) => {
+    this.setState({message: event.target.value});
+  }
+
+  handleSubmit = (event) => {
+    axios.post('https://script.google.com/macros/s/AKfycbworLNlQ8S56AetLC7RWDSwPlUNwP_Hv4pnity-hvnW_mym3c2e/exec', {
+      name: this.state.name,
+      email: this.state.email,
+      phone: this.state.phone,
+      message: this.state.message,
+    })
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+
+    event.preventDefault();
+  }
+// https://script.google.com/macros/s/AKfycbworLNlQ8S56AetLC7RWDSwPlUNwP_Hv4pnity-hvnW_mym3c2e/exec
   render() {
     return (
       <div className="row">
         <form
-          id="gform"
-          method="post"
-          action="https://script.google.com/macros/s/AKfycbworLNlQ8S56AetLC7RWDSwPlUNwP_Hv4pnity-hvnW_mym3c2e/exec"
+          onSubmit={this.handleSubmit}
           className="col s6"
           style={{paddingLeft: '40px'}}
         >
+          <input id="honeypot" name="honeypot" style={{display: 'none'}} />
           <div className="row">
             <div className="input-field col s12">
-              <input id="name" name="name" type="text" className="validate" />
-              <label for="name">Name</label>
+              <input id="name" value={this.state.name} onChange={this.handleNameChange} name="name" type="text" className="validate" />
+              <label htmlFor="name">Name</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <input id="email" name="email" type="text" className="validate" />
-              <label for="email" data-error="wrong">Email</label>
+              <input id="email" value={this.state.email} onChange={this.handleEmailChange} name="email" type="text" className="validate" />
+              <label htmlFor="email" data-error="wrong">Email</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <input id="phone" type="text" className="validate" />
-              <label for="phone">Phone Number</label>
+              <input id="phone" value={this.state.phone} onChange={this.handlePhoneChange} name="phone" type="text" className="validate" />
+              <label htmlFor="phone">Phone Number</label>
             </div>
           </div>
           <div className="row">
             <div className="input-field col s12">
-              <textarea id="message" name="message" className="materialize-textarea validate"></textarea>
-              <label for="message">Message</label>
+              <textarea id="message" value={this.state.message} onChange={this.handleMessageChange} name="message" className="materialize-textarea validate"></textarea>
+              <label htmlFor="message">Message</label>
             </div>
           </div>
           <div className="row">
@@ -44,6 +85,9 @@ export default class ContactForm extends Component {
             </div>
           </div>
         </form>
+        <div id="thankyou_message" className="col s6" style={{display: "none", paddingLeft: '40px'}}>
+          <h5>Thanks! I will contact you soon!</h5>
+        </div>
         <div className="col s4">
           Send me a message here!
         </div>
