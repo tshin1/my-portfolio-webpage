113b50f66220d7538917895596cffd4064e005e3
Ted Shin <tdyshin@gmail.com>
2018-01-30 18:29:37 -0500

    Cleaned up old materialize css stuff

--- src/App.js
+++ src/App.js
@@ -3,23 +3,11 @@ import 'materialize-css';
 // import './App.css';
 import style from './style.js';
 
-import NavBar from './components/NavBar';
-import About from './components/About';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
 export default class App extends Component {
   render() {
     return (
       <div>
-        <NavBar />
-        <div className="container" style={style.whiteBackground}>
-          <About />
-          <Portfolio />
-          <Contact />
-        </div>
-        <Footer />
+
       </div>
     );
   }
