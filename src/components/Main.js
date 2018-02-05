742d315fbdfe570477d3017c4298f0b23d78dc33
Ted Shin <tdyshin@gmail.com>
2018-02-04 20:57:56 -0500

    Add content to about section

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,43 +3,104 @@ import styled from 'styled-components';
 
 import './Main.css';
 
+// styled components
 const Section = styled.div`
   text-align: center;
   color: white;
+  // make min height equal to window size
   min-height: 100vh;
-`
-
-const AboutSection = Section.extend`
-  background-color: black;
   overflow: hidden;
+  // #####
+  // used to vertically center children
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  // #####
+`
+
+const Button = styled.button`
+
+`
+
+const PrimaryButton = Button.extend`
+  background-color: #428bca;
+`
+
+const DefaultButton = Button.extend`
+  color: black;
+  background-color: #FFFFFF;
+`
+
+// container that holds content for each Section
+const ContentContainer = styled.div`
+  max-width: 50vw;
+  border: 2px solid black;
+  padding: 2em 2em;
+`
+
+/*******************/
+/* Welcome Section */
+/*******************/
+const WelcomeSection = Section.extend`
+  background-color: black;
+
 `
 
 const Name = styled.h1`
   color: white;
+  font-size: 4em;
 `
 
-const AboutText = styled.h2`
+const WelcomeText = styled.h2`
   color: white;
+  font-size: 2em;
 `
 
+/*****************/
+/* About Section */
+/*****************/
+const AboutSection = Section.extend`
+  background-color: gray;
+
+`
+
+const AboutTitle = styled.h2`
+  color: white;
+  font-size: 2em;
+`
+
+const AboutText = styled.p`
+  color: white;
+  font-size: 1.5em;
+`
+
+/*********************/
+/* Portfolio Section */
+/*********************/
 const PortfolioSection = Section.extend`
-  background-color: gray;
+  background-color: black;
 `
 
+/********************/
+/* Projects Section */
+/********************/
 const ProjectsSection = Section.extend`
-  background-color: black;
-`
-
-const ContactSection = Section.extend`
   background-color: gray;
 `
 
-const LinksSection = Section.extend`
+/*******************/
+/* Contact Section */
+/*******************/
+const ContactSection = Section.extend`
   background-color: black;
+`
+
+/*****************/
+/* Links Section */
+/*****************/
+const LinksSection = Section.extend`
+  background-color: gray;
 `
 
 class Main extends Component {
@@ -50,9 +111,20 @@ class Main extends Component {
   render() {
     return (
       <div>
+        <WelcomeSection>
+          <ContentContainer>
+            <Name>Ted Shin</Name>
+            <WelcomeText>Hi, I'm a Software Engineer</WelcomeText>
+            <PrimaryButton>Let's Get Started</PrimaryButton>
+          </ContentContainer>
+        </WelcomeSection>
         <AboutSection>
-          <Name>Ted Shin</Name>
-          <AboutText>Hi, I'm a Software Engineer</AboutText>
+          <ContentContainer>
+            <AboutTitle>A Little About Me</AboutTitle>
+            <AboutText>I am a Full Stack Developer with expertise in building web applications, APIs, and backend systems in Javascript, React, Redux, Node, Sass, CSS, HTML, MongoDB, and Linux</AboutText>
+            <AboutText>I have a BS in Computer Science from CCNY</AboutText>
+            <DefaultButton>View My Portfolio</DefaultButton>
+          </ContentContainer>
         </AboutSection>
         <PortfolioSection>
 
